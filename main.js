fbaf32597b85c5a2eca60163251a66ecc45a89b1
HystERICKal <nyoroerick12@gmail.com>
2019-10-20 21:20:01 +0200

    git commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// console.log('Hello World');
+
+//search MDN (Mozilla Developer Network ) for more console commands
+
+//var - globally scoped (not used anymore)
+//let - block level scope
+//      -You can re-assign values
+//let age = 30;
+
+//const - block level scope
+//      -You cannot re-assign values 
